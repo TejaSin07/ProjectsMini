145567d1c04650e5f46ccf3edb37dc58abeddc87
tejas <tejassinkar10@gmail.com>
2025-06-18 12:26:18 +0530

    previously we are getting empty pdf so we did configuration in service imple now getting pdf with data

--- 01_ReportsApp/src/main/java/in/tejas/service/CitizenServiceImpl.java
+++ 01_ReportsApp/src/main/java/in/tejas/service/CitizenServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import com.lowagie.text.Document;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 import in.tejas.entity.CitizenPlan;
@@ -139,6 +140,28 @@ public class CitizenServiceImpl implements CitizenService {
 		document.open();
 		Paragraph p = new Paragraph("citizen plans info");
 		document.add(p);
+		
+		PdfPTable table = new PdfPTable(6);
+		table.addCell("Id");
+		table.addCell("Citizen Name");
+		table.addCell("Plan Name");
+		table.addCell("Plan Status");
+		table.addCell("Start Date");
+		table.addCell("End Date");
+		
+		
+		List<CitizenPlan> plans = planRepo.findAll();
+		
+		for(CitizenPlan plan : plans) {
+			table.addCell(String.valueOf(plan.getCitizenid()));
+			table.addCell(plan.getCitizename());
+			table.addCell(plan.getPlanName());
+			table.addCell(plan.getPlanStatus());
+			table.addCell(plan.getPlanStartDate()+"");
+			table.addCell(plan.getPlanEndDate()+"");
+	}
+		
+		document.add(table);
 		document.close();
 		return false;
 	}
